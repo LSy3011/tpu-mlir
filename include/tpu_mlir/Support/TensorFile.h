0a76671eb7c00ecf052626b4d02a3037da4f414e
LSy3011 <126790269+LSy3011@users.noreply.github.com>
2025-04-10 17:02:40 +0800

    Update TensorFile.h

--- include/tpu_mlir/Support/TensorFile.h
+++ include/tpu_mlir/Support/TensorFile.h
@@ -1,6 +1,5 @@
 //===- TensorFile.h - utilities for working with tensor files ------------*- C++
 //
-// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
 //
 // TPU-MLIR is licensed under the 2-Clause BSD License except for the
 // third-party components.
