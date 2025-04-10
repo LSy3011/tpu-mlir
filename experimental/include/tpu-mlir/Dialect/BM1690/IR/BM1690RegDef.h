b13a2a18dc9c428372af331f85ab6c1adc8fb9fd
LSy3011 <126790269+LSy3011@users.noreply.github.com>
2025-04-10 15:36:52 +0800

    Update BM1690RegDef.h

--- experimental/include/tpu-mlir/Dialect/BM1690/IR/BM1690RegDef.h
+++ experimental/include/tpu-mlir/Dialect/BM1690/IR/BM1690RegDef.h
@@ -1,7 +1,5 @@
 // ====- bm1690RefDef.cpp - BM1690 register definition ==========================
 //
-// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
-//
 // TPU-MLIR is licensed under the 2-Clause BSD License except for the
 // third-party components.
 //
