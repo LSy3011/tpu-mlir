62ff793444974c5f3a3c421dab219254688a8d6c
LSy3011 <126790269+LSy3011@users.noreply.github.com>
2025-04-10 17:35:04 +0800

    Update TgBnrPreprocess.cpp

--- lib/Backend/CV18xx/Kernel/TgBnrPreprocess.cpp
+++ lib/Backend/CV18xx/Kernel/TgBnrPreprocess.cpp
@@ -1,6 +1,5 @@
 //===----------------------------------------------------------------------===//
 //
-//
 // TPU-MLIR is licensed under the 2-Clause BSD License except for the
 // third-party components.
 //
