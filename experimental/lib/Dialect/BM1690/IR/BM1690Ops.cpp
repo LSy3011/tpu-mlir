3b2b01a3f2d8f08bf9ceb6d8bb65ed9806774919
LSy3011 <126790269+LSy3011@users.noreply.github.com>
2025-04-10 15:40:36 +0800

    Update BM1690Ops.cpp

--- experimental/lib/Dialect/BM1690/IR/BM1690Ops.cpp
+++ experimental/lib/Dialect/BM1690/IR/BM1690Ops.cpp
@@ -1,7 +1,5 @@
 //===- bm1690Ops.cpp - BM1690 operations  ---------------------------------===//
 //
-// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
-//
 // TPU-MLIR is licensed under the 2-Clause BSD License except for the
 // third-party components.
 //
